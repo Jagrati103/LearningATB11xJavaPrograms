d83755367c71333e6b47f2ab27dc280a8949de2f
Jagrati103 <shagunsri74@gmail.com>
2025-03-23 10:17:57 +0530

    Table of 9

---
+++ src/Practice_programs/Table_9_for_loop.java
@@ -0,0 +1,10 @@
+package Practice_programs;
+
+public class Table_9_for_loop {
+    public static void main(String[] args) {
+        int num = 9;
+        for (int i = 1; i <= 10; i++) {
+            System.out.printf("%d * %d = %d \n", num, i, num * i);
+        }
+    }
+}
