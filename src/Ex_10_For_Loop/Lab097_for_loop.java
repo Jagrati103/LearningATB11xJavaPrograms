fc9996eb0520db4049b68f8c06711df33ecfe667
Jagrati103 <shagunsri74@gmail.com>
2025-03-23 10:34:58 +0530

    Git Rep - commit

---
+++ src/Ex_10_For_Loop/Lab097_for_loop.java
@@ -0,0 +1,15 @@
+package Ex_10_For_Loop;
+
+public class Lab097_for_loop {
+    public static void main(String[] args) {
+        //for loop
+        //helps you to repeat a block of code
+        //initialization;condition;updation (increment/decrement)
+        //ICU
+        for(int i=5; i<10; i++){
+            System.out.println(i);
+            //Pre increment and post increment does not make much difference in case of for loop
+
+        }
+    }
+}
